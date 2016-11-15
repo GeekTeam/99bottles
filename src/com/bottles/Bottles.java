81c62091c3dd5c9ed340b6c71f11a6da25b65a2d
Ian <ian@right.here>
2016-11-15 22:26:53 +0000

    Convert the tests to java

---
+++ src/com/bottles/Bottles.java
@@ -0,0 +1,15 @@
+package com.bottles;
+
+public class Bottles {
+    public String verse(int i) {
+        return null;
+    }
+
+    public String verses(int i, int i1) {
+        return null;
+    }
+
+    public String song() {
+        return null;
+    }
+}
